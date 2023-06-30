97e655840d8724e8f1a16ba9b8ec040113e6a4b5
ivnviv <vanya0774@gmail.com>
2023-06-30 17:25:48 +0300

    qq

---
+++ src/shavermaStonks.java
@@ -0,0 +1,16 @@
+public class shavermaStonks {
+    public static void main(String[] args) {
+        float baseMoney = 70000F;
+        float pointMoney = 500000F;
+        float stonksPercent = 14.3F;
+
+        int monthCounter = 0;
+
+        while (baseMoney < pointMoney) {
+            baseMoney += ((baseMoney*stonksPercent)/100);
+            monthCounter++;
+
+        }
+        System.out.println("Для похода за шавермой понадобится " + monthCounter + " месяцев");
+    }
+}
