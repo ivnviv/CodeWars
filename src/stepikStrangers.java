d3ec2c091125d060e2fc8695760b8c5eb9564bfb
ivnviv <vanya0774@gmail.com>
2023-06-30 16:11:22 +0300

    qq

---
+++ src/stepikStrangers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class stepikStrangers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] numberAndNames = input.split(" ");
+        int number = Integer.parseInt(numberAndNames[0]);
+
+
+
+        if (number == 0) {
+            System.out.println("Оу... Похоже здесь никого...");
+        } else if (number < 0){
+            System.out.println("Серьезно? А что так негативно?");
+        } else {
+            for (int i = 1; i < numberAndNames.length; i++) {
+                String name = numberAndNames[i];
+                System.out.println("Привет, " + name);
+            }
+        }
+    }
+}
+
